54e21976670a5a90c7990de3820b2d3c0a09bc19
AlexWorkPC <demyanalex@gmail.com>
2022-11-22 12:03:18 +0300

    Задание 4-5

--- src/Main.java
+++ src/Main.java
@@ -22,5 +22,41 @@ public class Main {
         }else{
             System.out.println(year +" Не является високосным ");
         }
+        System.out.println(" Задание 4 ");
+        int deliveryDistance = 50;
+        int deliveryDays=1;
+        if(deliveryDistance>20){
+            deliveryDays++;
+        }
+        if(deliveryDistance>60){
+            deliveryDays++;
+        }
+        System.out.println(" Потребуется дней " + deliveryDays);
+        System.out.println(" Задание 5 ");
+        int monthNumber = 12;
+        switch (monthNumber){
+            case 12:
+            case 1:
+            case 2:
+                System.out.println(" Зимний сезон ");
+                break;
+            case 3:
+            case 4:
+            case 5:
+                System.out.println(" Весенний сезон ");
+                break;
+            case 6:
+            case 7:
+            case 8:
+                System.out.println(" Летний сезон ");
+                break;
+            case 9:
+            case 10:
+            case 11:
+                System.out.println(" Осенний сезон ");
+                break;
+            default:
+                System.out.println(" Такого месяца нет ");
+        }
     }
 }
