7bc652a81c9e1e268d1d07e1b748ab50eaafbab9
AlexWorkPC <demyanalex@gmail.com>
2022-11-22 09:35:47 +0300

    Задание 1

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Задание 1");
+        int clientOS = 0;
+        if (clientOS==1){
+            System.out.println(" Установите версию клиента для Android ");
+        }else {
+            System.out.println(" Установите версию клиента IOS ");
+        }
+    }
+}
