7adb1753df63ea104053c042b918498d22cb22a4
AlexWorkPC <demyanalex@gmail.com>
2022-11-22 09:49:28 +0300

    Задание 2

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,19 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Задание 1");
-        int clientOS = 0;
-        if (clientOS==1){
+        int clientOS = 1;
+        int clientDeviceYear = 2015;
+        if (clientOS == 1) {
             System.out.println(" Установите версию клиента для Android ");
-        }else {
-            System.out.println(" Установите версию клиента IOS ");
+            if (clientDeviceYear < 2015) {
+                System.out.println(" Установите облегченную версию для Android ");
+            }
+        } else {
+            if (clientDeviceYear < 2015) {
+                System.out.println(" Установите облегченную версию клиента IOS ");
+            } else {
+                System.out.println(" Установите версию клиента IOS ");
+            }
         }
     }
 }
